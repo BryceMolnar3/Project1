8b0827a2c0d74cc4a37fd422103a05e2bd83ff59
BryceMolnar <bmolnar@hamilton.edu>
2022-09-11 17:26:50 -0400

    This is going back to master

--- Project0.cc
+++ Project0.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+// This is my new better comment
 int main() {
   std::string Program = "#include <iostream>#include <cstring>int main() {  std::string Program =   std::cout << Program.substr(0,19) << '\\n' << Program.substr(19,18) << '\\n' << Program.substr(37,12) << '\\n' << Program.substr(49,24) << '\"' << Program << '\"' << ';' << '\\n' << Program.substr(73,234) << '\\n' << Program.substr(307,3);};";
   std::cout << Program.substr(0,19) << '\n' << Program.substr(19,18) << '\n' << Program.substr(37,12) << '\n' << Program.substr(49,24) << '"' << Program << '"' << ';' << '\n' << Program.substr(73,234) << '\n' << Program.substr(307,3);
